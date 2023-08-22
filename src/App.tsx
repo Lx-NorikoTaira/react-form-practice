bf0e8c0c204d1e561c02fe68705470e7417cc44f
Lx-NorikoTaira <noriko.taira@4digit.jp>
2023-08-22 15:56:13 +0900

    react-form-practice

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { validationSchema } from "./utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface Loginform {
+  name: string;
+  email: string;
+  password: string;
+}
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Loginform>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit = (data: Loginform) => {
+    console.log(data);
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='名前'>名前</label>
+        <input id='name' type='text' {...register("name")} />
+        <p>{errors.name?.message as React.ReactNode}</p>
+        <label htmlFor='メールアドレス'>メールアドレス</label>
+        <input id='email' type='email' {...register("email")} />
+        <p>{errors.email?.message as React.ReactNode}</p>
+        <label htmlFor='パスワード'>パスワード</label>
+        <input id='password' type='password' {...register("password")} />
+        <p>{errors.password?.message as React.ReactNode}</p>
+        <button type='submit'>送信</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
